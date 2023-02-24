8eb1b9d0f1fb3ffd2b0a5ee95a32c0cbb7f2e6a1
Yasin Yılmaz <yasinyilmazzz@gmail.com>
2023-02-24 09:49:00 +0300

    0224

--- src/test/java/pages/MyAppointmentsPage.java
+++ src/test/java/pages/MyAppointmentsPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class MyAppointmentsPage {
     public MyAppointmentsPage() {
         PageFactory.initElements(Driver.getDriver(),this);
@@ -13,12 +15,66 @@ public class MyAppointmentsPage {
     @FindBy(id = "appointment-heading")
     public WebElement appointmentHeading;
 
-    @FindBy(xpath = "//span[@class='d-none d-md-inline'][1]")
+    @FindBy(xpath = "//span[@class='d-none d-md-inline']")
+    public List<WebElement> editButtonList;
+
+    @FindBy(partialLinkText = "appointment-update")
     public WebElement firstEditButton;
 
-    @FindBy(xpath = "//span[@class='d-none d-md-inline'][1]")
+    @FindBy(xpath = "(//span[.='Edit'])[3]")
     public WebElement secondEditButton;
 
-    @FindBy(xpath = "//span[@class='d-none d-md-inline'][1]")
+    @FindBy(xpath = "(//span[.='Edit'])[5]")
     public WebElement thirdEditButton;
+
+    @FindBy(id = "hospitalmsappfrontendApp.appointment.home.createOrEditLabel")
+    public WebElement createOrEditHeader;
+
+    @FindBy(name = "id")
+    public WebElement id;
+
+    @FindBy(name = "startDate")
+    public WebElement startDate;
+
+    @FindBy(name = "endDate")
+    public WebElement endDate;
+
+    @FindBy(id = "appointment-physician")
+    public WebElement physician;
+
+    @FindBy(id = "appointment-status")
+    public WebElement status;
+
+    @FindBy(name = "anamnesis")
+    public WebElement anamnesis;
+
+    @FindBy(name = "treatment")
+    public WebElement treatment;
+
+    @FindBy(name = "diagnosis")
+    public WebElement diagnosis;
+
+    @FindBy(id = "appointment-prescription")
+    public WebElement prescription;
+
+    @FindBy(id = "appointment-description")
+    public WebElement description;
+
+    @FindBy(xpath = "//span[.='Save']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[@class='invalid-feedback']")
+    public WebElement requiredFieldMessage;
+
+    @FindBy(xpath = "//option[@value='PENDING']")
+    public WebElement pending;
+
+    @FindBy(xpath = "//option[@value='COMPLETED']")
+    public WebElement completed;
+
+    @FindBy(xpath = "//option[@value='CANCELLED']")
+    public WebElement cancelled;
+
+    @FindBy(xpath = "//option[@value='UNAPPROVED']")
+    public WebElement unapproved;
 }
--- src/test/java/stepdefinitions/UITestStepDefs/HomePageStepDefs.java
+++ src/test/java/stepdefinitions/UITestStepDefs/HomePageStepDefs.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.When;
 import pages.HomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class HomePageStepDefs {
     HomePage homePage = new HomePage();
@@ -16,10 +17,12 @@ public class HomePageStepDefs {
     @When("{string} clicks account menu dropdown")
     public void clicks_account_menu_dropdown(String string) {
         homePage.userIcon.click();
+        ReusableMethods.waitFor(1);
     }
     @When("{string} clicks sign in button")
     public void clicks_sign_in_button(String string) {
         homePage.signInOption.click();
+        ReusableMethods.waitFor(1);
     }
 
     @Then("Admin close the app")
